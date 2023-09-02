d0eaacb1de768c704b98bfeb65cd2dbb651eb9b5
Raj <SoftDevRaj77@gmail.com>
2023-09-02 15:38:25 -0400

    Created Index.JS and created Skeleton Code

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+
+// Import necessary modules (e.g., Inquirer, your shape classes, etc.)
+// Fill these in as I create them.
+// const inquirer = require('inquirer');
+// const { Triangle, Circle, Square } = require('./lib/shapes');
+
+// Start the application
+function startApp() {
+    // TODO: Prompt user for text, text color, shape, and shape color using Inquirer
+    
+    // TODO: Based on user input, create the desired shape object (Triangle, Circle, Square)
+    
+    // TODO: Generate SVG string using the shape object's render() method
+    
+    // TODO: Save SVG string to a file named logo.svg
+    
+    // TODO: Print a confirmation message to the user
+}
+
+// Invoke the startApp function to run the application
+startApp();
